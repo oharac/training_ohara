2cc0b97d8d14e34489d34aac270b70c3e5a6ec23
oharac <ohara@nceas.ucsb.edu>
2024-10-08 10:22:14 -0700

    Initial commit

---
+++ scripts/intro_to_r.R
@@ -0,0 +1,14 @@
+important_value <- 3 * 4
+weight_kg <- 25
+weight_lb <- 2.2 * weight_kg
+
+### creating a vector
+weight_kg <- c(25, 33, 12)
+weight_lb <- 2.2 * weight_kg
+
+
+chr_vec <- c('hello', 'good bye', 'see you later')
+num_vec <- c(5, 1.3, 10)
+lgl_vec <- c(TRUE, FALSE, TRUE)
+
+weight_lb <- mean(weight_lb)
